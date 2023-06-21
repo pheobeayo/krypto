c877deb059bf1c69f252d295e4bb11b403d05426
Oji Ubasi <ojiinnocent@gmail.com>
2023-06-21 15:39:51 +0100

    added some installations for thirdweband vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(),],
+//   define: {
+//     // By default, Vite doesn't include shims for NodeJS/
+//     // necessary for segment analytics lib to work
+//     global: {},
+//   },
+// })
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react(), nodePolyfills()],
   define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
+    "process.env": {},
   },
-})
+});
